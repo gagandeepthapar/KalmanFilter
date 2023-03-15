7ad43e0694b75b72c11a31931705ee90f87cbbb4
gaganthapar12309 <gsthapar@gmail.com>
2023-03-15 11:15:19 -0700

    updated readme with correct LTI system. implemented LTI in python

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# KalmanFilter
-Exploration of Kalman Filters in Python and C++. Part of a larger project to simulate rocket landings in faster-than-real-time in C++. This repository will hold some more theory/background on the Kalman Filter specifically as it relates to data handling and real-time data smoothing. This system analyzes a standard mass-spring-dampening system in the LTI form $\dot{x} = Ax + Bu$.
+# Kalman Filter
+Exploration of Kalman Filters in Python and C++. Part of a larger project to simulate rocket landings in faster-than-real-time in C++. This repository will hold some more theory/background on the Kalman Filter specifically as it relates to data handling and real-time data smoothing. This system analyzes a standard mass-spring-dampening system in the LTI form $\dot{x} = Ax + Bu + Lw$ and $y = Cx + Du + Mv$. Here, $w$ ~ $\aleph(0, Q)$ and $v$ ~ $\aleph(0, R)$ where $Q$ is the covariance of the input state (noise in input) and $R$ is the covariance of the observation method (noise in measurement).
 
 ## The System
 For arbitrary spring constant, $k$, dampening coefficient, $c$, mass, $m$, and input force, $F$, the system dynamics can be written as 
@@ -22,13 +22,10 @@ x \\
 s_1 \\
 s_2
 \end{bmatrix}
-\quad u =  \begin{bmatrix}
-0 \\
-F
-\end{bmatrix} = \begin{bmatrix}
-u_1 \\
-u_2
-\end{bmatrix}
+$$
+
+$$
+\bar{u} = \begin{bmatrix} F \end{bmatrix} = \begin{bmatrix} u_1 \end{bmatrix}
 $$
 
 $$
@@ -40,9 +37,54 @@ $$
 \frac{F - c\dot{x} - kx}{m}
 \end{bmatrix} = \begin{bmatrix}
 s_2 \\
-\frac{1}{m}\left(u_2 - cs_2 - ks_1\right)
+\frac{1}{m}\left(u_1 - cs_2 - ks_1\right)
 \end{bmatrix}
 $$
+
+Looking at the formulation for $\dot{\bar{x}}$, the state matrices $A, B, C, D$ need to be determined in terms of $\bar{x}$. In fear of being too verbose, $A, B, C,$ and $D$ are shown below. Multiplying the matrices by the correct factor will prove the formulations hold true with the form $\dot{x} = Ax + Bu$ and $y = Cx + Du$.
+
+$$
+A = \begin{bmatrix}
+0 & 1 \\
+-\frac{k}{m} & -\frac{c}{m}
+\end{bmatrix}
+$$
+
+$$
+B = \begin{bmatrix}
+0 \\
+\frac{1}{m}
+\end{bmatrix}
+$$
+
+
+$$
+L = \begin{bmatrix}
+1 & 0 \\
+0 & 1
+\end{bmatrix}
+$$
+
+$$
+C = \begin{bmatrix}
+1 & 0 \\
+0 & 1
+\end{bmatrix}
+$$
+
+$$
+D = \begin{bmatrix} 0 \end{bmatrix}
+$$
+
+$$
+M = \begin{bmatrix}
+1 & 0 \\
+0 & 1
+\end{bmatrix}
+$$
+
+Notice: the system has full observability as shown by $C$, that is, the system can directly measure the full state, albeit, with some noise as modeled by $v$.
+
 
 While this can be solved analytically using the [state transformation](https://web.mit.edu/2.14/www/Handouts/StateSpaceResponse.pdf), another method is to propagate the system manually (e.g., with a Runge Kutta solver) and keep track of the states and inputs. This is what this mini-project will do to explore Kalman Filtering as it requires the system dynamics be known and used during the prediction step.
 
--- kalman.py
+++ kalman.py
@@ -15,8 +15,64 @@ class KalmanFilter:
 # representing mass-spring system
 class DynamicSystem:
 
-    def __init__(self):
+    def __init__(self, A:np.ndarray,
+                       B:np.ndarray,
+                       C:np.ndarray,
+                       D:np.ndarray,
+                       Q:np.ndarray,
+                       R:np.ndarray,
+                       L:np.ndarray,
+                       M:np.ndarray,
+                       x0:np.ndarray):
+
+        """
+        setup LTI system:
+        xDot = Ax + Bu + Lw
+        y = Cx + Du + Mv
+        """
+        
+        self.A = A
+        self.B = B
+        self.L = L
+        self.Q = Q
+        
+        self.C = C
+        self.D = D
+        self.M = M
+        self.R = R
+        
+
+        self.ideal_state:np.ndarray = x0    # no noise
+        self.real_state:np.ndarray = x0 # input noise
+        self.meas_state:np.ndarray = x0     # input and obs noise
+
         return
+    
+    def propagate(self, delT:float, input:np.ndarray)->np.ndarray:
+
+        self.ideal_state = self.__update_state(delT, self.ideal_state, input, noise=np.zeros(self.Q.shape))
+        self.real_state = self.__update_state(delT, self.real_state, input, noise=self.Q)
+        self.meas_state = self.__measure_state(input)
+    
+        return self.meas_state
+    
+    
+    def __update_state(self, delT:float, state:np.ndarray, input_control:np.ndarray, noise:np.ndarray)->np.ndarray:
+
+        W = np.array([np.random.normal(covar) for covar in noise])
+        
+        del_state = self.A @ state + self.B @ input_control + self.L @ W
+
+        new_state = state + del_state*delT
+
+        return new_state
+    
+    def __measure_state(self, input_control:np.ndarray)->np.ndarray:
+
+        V = np.array([np.random.normal(covar) for covar in self.R])
+        obs_state = self.C @ self.real_state + self.D @ input_control + self.M @ V
+
+        return obs_state
 
 def plot_results(time:np.ndarray, full_data_set:np.ndarray[np.ndarray[float]], labels:np.ndarray[str],
                  *, num_rows:int=None, num_cols:int=1)->None:
@@ -46,16 +102,61 @@ def plot_results(time:np.ndarray, full_data_set:np.ndarray[np.ndarray[float]], l
 
     return
 
-def state_transition(del_time:float, state:np.ndarray)->np.ndarray:
-
-    return
-
 def simulate():
     
     # arbitrary timing
     time = np.linspace(0, 10, 10001)
+    input = np.sin(2*np.pi * time / 2.5)    # arbitrary input
+
+    x0 = np.array([5, 0])
+    cart_A = np.array([[0, 1], [-SPRING/MASS, -DAMPEN/MASS]])
+    cart_B = np.array([0, 1/MASS])   # see README for derivation
+    cart_L = np.eye(len(x0))
+    Q = np.array([0.001, 0.001])  # accurate control on input
+
+    cart_C = np.eye(len(x0))    # assume full observability
+    cart_D = np.zeros(len(x0))         # assume no observation input
+    cart_M = np.eye(len(x0))
+    R = np.array([0.001, .01])   # relatively noise data
+
+    cart_system = DynamicSystem(cart_A, cart_B, cart_C, cart_D, Q, R, cart_L, cart_M, x0)
+
+    ideal_history = np.zeros((2, len(time))).T
+    real_history = np.zeros((2, len(time))).T
+    meas_history = np.zeros((2, len(time))).T
+
+    ideal_history[0] = cart_system.ideal_state
+    real_history[0] = cart_system.real_state
+    meas_history[0] = cart_system.meas_state
+
+    for i in range(1, len(time)):
+        delt = time[i] - time[i-1]
+        cart_system.propagate(delt, np.array([0,input[i]]))
+
+        ideal_history[i] = cart_system.ideal_state
+        real_history[i] = cart_system.real_state
+        meas_history[i] = cart_system.meas_state
+
+    fig = plt.figure()
+    ax = fig.add_subplot(311)
+    ax.plot(time, ideal_history.T[0], label='Pos')
+    ax.plot(time, ideal_history.T[1], label='Vel')
+    ax.set_title('Ideal')
+    ax.legend()
+
+    ax = fig.add_subplot(312)
+    ax.plot(time, real_history.T[0], label='Pos')
+    ax.plot(time, real_history.T[1], label='Vel')
+    ax.set_title('Real')
+    ax.legend()
     
-    f = 0.3*np.sin(2*np.pi*time/2.5)    # input force, u
+    ax = fig.add_subplot(313)
+    ax.plot(time, meas_history.T[0], label='Pos')
+    ax.plot(time, meas_history.T[1], label='Vel')
+    ax.set_title('Measured')
+    ax.legend()
+
+    plt.show()
 
     return
 
