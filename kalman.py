e2f59ae0f52dc9f7c016272552611ffd76e36622
gaganthapar12309 <gsthapar@gmail.com>
2023-03-14 21:54:22 -0700

    changed setup to match LTI system instead of random functions

--- kalman.py
+++ kalman.py
@@ -1,73 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-class ExtendedKalmanFilter:
+# arbitrary system constants
+MASS = 5   
+SPRING = 10
+DAMPEN = 7
 
-    def __init__(self, x0:np.ndarray=None, P0:np.ndarray=None):
+# KF class
+class KalmanFilter:
 
-        self.xHat = x0
-        self.P = P0
-
+    def __init__(self):
         return
     
-    def add_measurement(self, measurement:np.ndarray)->np.ndarray:
+# representing mass-spring system
+class DynamicSystem:
 
+    def __init__(self):
+        return
 
+def plot_results(time:np.ndarray, full_data_set:np.ndarray[np.ndarray[float]], labels:np.ndarray[str],
+                 *, num_rows:int=None, num_cols:int=1)->None:
+    """
+    Simple utility function to plot data points
 
-        return self.xHat
-    
-
-def plot_results(time:np.ndarray, true:np.ndarray, measured:np.ndarray, filtered:np.ndarray)->None:
-
-    num_states = true.shape[0]
+    Args:
+        time (np.ndarray): time array
+        full_data_set (np.ndarray[np.ndarray[float]]): array of datasets 
+        labels (np.ndarray[str]): list of labels in order
+        num_rows (int, optional): number of rows in figure. Defaults to None.
+        num_cols (int, optional): number of cols in figure. Defaults to 1.
+    """
+    if num_rows is None:
+        num_rows = full_data_set.shape[0]
 
     fig = plt.figure()
     
-    for idx in range(num_states):
-        ax = fig.add_subplot(num_states, 2, 2*idx + 1)
-
-        ax.plot(time, true[idx], c='g', label='True')
-        ax.plot(time, measured[idx], c='b', label='Measured from Sensor', alpha=0.3)
-        ax.plot(time, filtered[idx], c='k', linestyle='dashdot', label='Kalman Filtered')
-        ax.legend()
-        ax.set_title('State {}'.format(idx+1))
-        
-        ax = fig.add_subplot(num_states, 2, 2*idx+2)
-        ax.plot(time, np.zeros(time.shape), c='r', linestyle='--', label='True')
-        ax.plot(time, true[idx] - measured[idx], c='b', label='Error in Measurement', alpha=0.3)
-        ax.plot(time, true[idx] - filtered[idx], c='k', linestyle='dashdot', label='Error in Filter')
-        ax.legend()
-        ax.set_title('State {}: Error'.format(idx+1))
-
-        
+    for i in range(num_rows):
+        for j in range(num_cols):
+            idx = num_cols * i + j
+            ax = fig.add_subplot(num_rows, num_cols, idx + 1)
+            ax.plot(time, full_data_set[idx], label=labels[idx])
+            ax.legend()
 
     plt.show()
 
     return
 
+def state_transition(del_time:float, state:np.ndarray)->np.ndarray:
+
+    return
+
 def simulate():
-
-    covarA = 0.05
-    covarB = 0.2
-    covarC = 0.1
-
+    
     # arbitrary timing
-    time = np.linspace(0, 100, 1001)
+    time = np.linspace(0, 10, 10001)
     
-    trueA = np.sin(2*np.pi*time / 25)
-    trueB = np.cos(2*np.pi*time / 50)
-    trueC = np.exp(-time * 0.1)
-    
-    measA = trueA + np.random.normal(0, covarA, trueA.shape)
-    measB = trueB + np.random.normal(0, covarB, trueB.shape)
-    measC = trueC + np.random.normal(0, covarC, trueC.shape)
-
-    true = np.vstack([trueA, trueB, trueC])
-    meas = np.vstack([measA, measB, measC])
-    filt = np.zeros(true.shape)
-
-    kf = ExtendedKalmanFilter()
-    plot_results(time, true, meas, filt)
+    f = 0.3*np.sin(2*np.pi*time/2.5)    # input force, u
 
     return
 
