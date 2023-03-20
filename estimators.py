189c075be7eb34aa197f433a027d69b772647a16
gaganthapar12309 <gsthapar@gmail.com>
2023-03-20 15:10:07 -0700

    adding class files for all state estimators (ideal and kalman)

---
+++ estimators.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+class StateEstimator:
+
+    def __init__(self):
+
+        return
+
+    def __repr__(self)->str:
+        return "Ideal Estimator"
+
+
+# KF class
+class KalmanFilter(StateEstimator):
+
+    def __init__(self):
+
+        return
+    
+    def __repr__(self)->str:
+        return "Kalman Filter"
+    
